087133f92da0e9afae1099e0fe62fbbe1b467ca4
Deon Kim <deonquebec@gmail.com>
2016-07-23 22:14:54 +0900

    flashcard minor

--- pracpy/codingclub/myFlashCards/myFlashCards.py
+++ pracpy/codingclub/myFlashCards/myFlashCards.py
@@ -1,16 +1,33 @@
+from decimal import *
 from tkinter import *
+import csv
+import decimal
 
 # Main
 
 my_window = Tk()
 my_window.title("My Flashcard Application")
 
-## declare variables
+## declare variables to store quizzes from the first column of the csv file, and to store answers from the second column.
 
 quizzes=[]
 answers=[]
 
-# Get quiz 
+# read myFlashcards
+
+with open("flashcards.csv") as mycsv:
+    myreader = csv.reader(mycsv)
+
+    for row in myreader:
+        quizzes.append(row[0])
+        answers.append(row[1])
+
+## Just to debug the above reading script
+
+    print(quizzes)
+    print(answers)
+        
+# Get quiz function
 
 my_index=-1
 print("my_index:",my_index)
@@ -26,27 +43,24 @@ def click_quiz ():
         aquiz=quizzes[ my_index ]
         my_quizoutput.insert(END,aquiz)
 
-# Get ans
+# Get ans function
 
 def click_ans():
     my_ansoutput.delete(0.0,END)
     definition=answers[ my_index ]
     my_ansoutput.insert(END,definition)
     
-
-
 # Button: Quiz
 
-Button(my_window, text="get quiz", width=5, command=click_quiz).grid(row=0, column=0, sticky=E)
+Button(my_window, text="get quiz", width=10, height =2, command=click_quiz).grid(row=0, column=0, sticky=E)
 
 # Button: Answer
 
-Button (my_window, text="get ans", width=5, command=click_ans).grid(row=0, column=1, sticky=W)
-### column = 0 suspicious
+Button (my_window, text="get ans", width=10, height=2, command=click_ans).grid(row=0, column=1, sticky=W)
 
 # quiz output Textbox
 
-my_quizoutput=Text(my_window, width=75, height = 2, wrap=WORD, background="light yellow")
+my_quizoutput=Text(my_window, width=150, height = 5, wrap=WORD, background="light yellow")
 my_quizoutput.grid(row=1,column=0, columnspan=2)
 ### sticky=E excluded
 
@@ -56,24 +70,10 @@ Label(my_window, text="Definition:").grid(row=2,column=0, sticky=W)
 
 # ans output Textbox
 
-my_ansoutput=Text(my_window, width=75, height=6, wrap=WORD, background="light yellow")
-my_ansoutput.grid(row=4,column=0,columnspan=2, sticky=W)
+my_ansoutput=Text(my_window, width=150, height=10, wrap=WORD, background="light yellow")
+my_ansoutput.grid(row=3,column=0,columnspan=2, sticky=W)
 ### columnspan=2 not included
 
-# read myFlashcards
-
-import csv
-
-with open("myflashcards.csv") as mycsv:
-    myreader = csv.reader(mycsv)
-
-    for row in myreader:
-        quizzes.append(row[0])
-        answers.append(row[1])
-
-    print(quizzes)
-    print(answers)
-        
 
 # mainloop
 
