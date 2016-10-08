20c5962314824faec824cadf2a6100d8af52853e
Deon Kim osX <dae.h.kim@mail.mcgill.ca>
2016-10-08 01:42:36 -0400

    migrate manual to ~/mymanual

---
+++ praccpp/udemy/forbeginners/Outputting Text/OutputtingText.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Starting program..."<<flush;
+    cout << "this is some text."<<endl;
+    cout << "Banana.  "<< "apple." << "  Orange." << endl;
+    cout << "This is some more text."<<endl;
+    
+    return 0;
+}
+
