c08cc39f2b13302de49bc24cb90e2130f552511d
DQmacOS <deonquebec@gmail.com>
2017-04-14 00:56:41 -0400

    busy

---
+++ c/test/hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("hello world!\n");
+    int x = 5;
+    int y = 5;
+    for(int i=0;i<y;i++){
+        x=x+i;
+        printf("%d\n",x);
+
+    }
+
+    return 0;
+}
